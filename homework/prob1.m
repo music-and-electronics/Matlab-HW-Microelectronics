a6a777bc74879566d6e00274858e448d44ccfcf8
김종현 <kjh970407@naver.com>
2019-11-08 16:33:38 +0900

    plot changed

--- homework/prob1.m
+++ homework/prob1.m
@@ -27,8 +27,8 @@ close all;
         V_BE =V_t*log(I_C/I_s);                     %update V_BE from iterated I_C
         err  =err-V_BE;                             %err = V_BE-V_BE_new
     end
-    V_B =V_BE+V_E;
-    R_C_max=(V_cc-V_B)/I_C;
+    V_B =V_BE+V_E;                                  %V_BE is V_B - V_E
+    R_C_max=(V_cc-V_B)/I_C;                         %Rc is max when V_C = V_B
 
     disp(I_C); disp(V_B); disp(R_C_max);
 
@@ -36,38 +36,38 @@ close all;
     R_c     =1e3;
     R_L     =10e3;
     R_src   =500;
-    R_T     =P_Resistance(R_bias,R_src);
+    R_T     =P_Resistance(R_bias,R_src);           %value of thevenin equivalent resistance
 %-Voltage-%
     V_A     =50;
 %-etc-%
-    gm      =I_C/V_t;
-    r_o     =V_A/I_C;
+    gm      =I_C/V_t;                              %transconductance
+    r_o     =V_A/I_C;                              %Early Resistance
 %-(b)-%
-    R_in    = R_bias/(R_src+R_bias);
-    gain    =-(P_Resistance(R_c,R_L,r_o)/((1/gm)+R_T/(1+beta)))*R_in;
+    R_in    = R_bias/(R_src+R_bias);               %value of input impedence
+    gain    =-(P_Resistance(R_c,R_L,r_o)/((1/gm)+R_T/(1+beta)))*R_in; %gain calculation in CE circuit
     disp(gain);
 %-(c)-%
-    t       =[0:1e-10:3e-4]';
-    freq    =10e3;
-    V_in    =1e-3*sin(2*pi*freq.*t);
-    V_dc= V_cc-I_C*R_c;
-    V_c1    =gain*V_in+V_dc;
-    
+    t       =[0:1e-10:3e-4]';                      %time scale definition
+    freq    =10e3;                                 %freqeuncy definition
+    V_in    =1e-3*sin(2*pi*freq.*t);               %V_in definition
+    V_dc    = V_cc-I_C*R_c;                        %value of DC voltage in large signal model
+    V_c1    =gain*V_in+V_dc;                       %actual value of V_c1 with the effect of DC voltage
+  
+%-plot code of prob(c)-%
     figure;
-    plot(t,V_c1,'LineWidth',2);
+    plot(t,V_c1,'LineWidth',2);                    %characteristic of graph line
     grid on
-    title('The Plot Of Vc1');
-    num     =3e-4;
-    xlabel(['time[s] : 0<t<',num2str(num)],'fontsize',12);
-    ylabel('Vc1[V]','fontsize',12);
-    legend({'Vc1'},'Location','southwest');
+    title('The Plot Of Vc1 (V/time)');             %title of plot
+    xlabel('time[s] (3 cycles)','fontsize',12);    %explain of x-axis
+    ylabel('Vc1[V]','fontsize',12);                %explain of y-axis
+    legend({'Vc1'},'Location','northeast');        %position of graph name label
 
 %-(d)-%
-    gain_taget  =-50;
-    error_gain  =10;
+    gain_taget  =-50;                              %initial target value of gain
+    error_gain  =10;                               %initial gain assumption
 
-    while(abs(error_gain)>abs(gain_taget/100))
-        V_bias     =(R_b2/(R_b1+R_b2))*V_cc; 
+    while(abs(error_gain)>abs(gain_taget/100))     %if error value is less or same with 1% of target gain
+        V_bias     =(R_b2/(R_b1+R_b2))*V_cc;       
         R_bias     =P_Resistance(R_b1,R_b2);
         I_B        =(V_bias-V_BE)/R_bias;  
         I_C        =I_B*beta;        
@@ -76,12 +76,12 @@ close all;
         gm         =I_C/V_t;        
         R_in       =R_bias/(R_src+R_bias);
         R_T        =P_Resistance(R_bias,R_src);
-        gain_cal   =-(P_Resistance(R_c,R_L,r_o)/((1/gm)+R_T/(1+beta)))*R_in;
-        error_gain =gain_cal-gain_taget;
+        gain_cal   =-(P_Resistance(R_c,R_L,r_o)/((1/gm)+R_T/(1+beta)))*R_in;    %updating gain by updated gain factors
+        error_gain =gain_cal-gain_taget;                                        %updating gain error value by updated gain
         
         if error_gain>0 %if gain_target is larger than gain_cal
             R_b2 =R_b2+0.1; 
-        else            %else if gain_cal is larger than gain target
+        else            %else gain_cal is larger than gain target
             R_b2 =R_b2-0.1;
         end
 
